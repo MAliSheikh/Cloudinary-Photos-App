c2fbc0bca60712119356a8a0d22bc7bc6be522fd
MAliSheikh <malisheikhg772@gmail.com>
2023-09-03 00:52:44 +0500

    Album shown on page

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 // import GalleryGrid from '@/app/gallery/galleryGrid';
 import cloudinary from 'cloudinary'
+import { AlbumCard } from './albumCard';
 
-export interface searchResult {
-    public_id: string;
-    tags: string[];
+export interface Folder {
+    name: string; path: string;
 }
 
 export default async function AlbumsPage() {
-    const results = (await cloudinary.v2.search
-        .expression('resource_type:image')
-        .sort_by('created_at', 'desc')
-        .with_field('tags')
-        .max_results(100000)
-        .execute()) as { resources: searchResult[] }
+    const { folders } = await cloudinary.v2.api.root_folders() as {
+        folders: Folder[];
+    }
 
     return (
         <section>
             <div className='flex flex-col gap-8'>
                 <div className='flex justify-between'>
                     <h1 className='text-4xl font-bold'>Albums</h1>
+                </div>
+                <div className='grid grid-cols-3 gap-4'>
+                    {folders.map((folder) => (
+                        <AlbumCard key={folder.path} folder={folder} />
+                    ))}
                 </div>
             </div>
         </section>
