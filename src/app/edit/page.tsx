ac984cab2f60c3c36bad3d5840cfe2f270da46e1
MAliSheikh <malisheikhg772@gmail.com>
2023-09-04 10:57:16 +0500

    AI Pic efiting added Added

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -8,7 +8,7 @@ export default function EditPage({ searchParams: { publicId }, }:
     { searchParams: { publicId: string } }
 ) {
     const [transformation, setTransformation] = useState<
-        undefined | "generative-fill"
+        undefined | "generative-fill" | 'green' | 'blur' | 'grayscale' | 'pixelate' | 'border'
     >()
 
     return (
@@ -18,12 +18,34 @@ export default function EditPage({ searchParams: { publicId }, }:
                     <h1 className='text-4xl font-bold'>Edit {publicId}</h1>
                 </div>
 
-                <Button onClick={
-                    () => {
-                        setTransformation('generative-fill')
-                    }
-                }
-                    className="bg-white text-black hover:bg-white">Generative Fill</Button>
+                <div className="flex  gap-4">
+                    <Button onClick={() => { setTransformation(undefined) }}
+                        className="rounded bg-white text-black hover:bg-white">
+                        Clear All
+                    </Button>
+
+                    <Button onClick={() => { setTransformation('generative-fill') }}
+                        className="rounded bg-white text-black hover:bg-white">
+                        Apply Generative Fill
+                    </Button>
+                    <Button onClick={() => { setTransformation('blur') }}
+                        className="rounded bg-white text-black hover:bg-white">
+                        Apply Blur
+                    </Button>
+                    <Button onClick={() => { setTransformation('grayscale') }}
+                        className="rounded bg-white text-black hover:bg-white">
+                        Convert to Gray
+                    </Button>
+                    <Button onClick={() => { setTransformation('pixelate') }}
+                        className="rounded bg-white text-black hover:bg-white">
+                        Pixelate
+                    </Button>
+                    <Button onClick={() => { setTransformation('border') }}
+                        className="rounded bg-white text-black hover:bg-white">
+                        Border
+                    </Button>
+
+                </div>
 
                 <div className="grid grid-cols-2 gap-12">
                     <CldImage
@@ -43,6 +65,43 @@ export default function EditPage({ searchParams: { publicId }, }:
                             fillBackground
                         />
                     }
+                    {transformation === 'blur' &&
+                        <CldImage
+                            src={publicId}
+                            width={300}
+                            height={200}
+                            blur={'1200'}
+                            alt="Some Image"
+                        />
+                    }
+                    {transformation === 'grayscale' &&
+                        <CldImage
+                            src={publicId}
+                            width={300}
+                            height={200}
+                            grayscale
+                            alt="Some Image"
+                        />
+                    }
+                    {transformation === 'pixelate' &&
+                        <CldImage
+                            src={publicId}
+                            width={300}
+                            height={200}
+                            pixelate
+                            alt="Some Image"
+                        />
+                    }
+                    {transformation === 'border' &&
+                        <CldImage
+                            src={publicId}
+                            width={300}
+                            height={200}
+                            border="40px_solid_purple"
+                            alt="Some Image"
+                        />
+                    }
+
                 </div>
             </div>
         </section>)
