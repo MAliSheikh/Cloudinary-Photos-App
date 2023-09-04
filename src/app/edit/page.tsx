6c3cf9e6149fa5e027cf054468ce5ffa300133db
MAliSheikh <malisheikhg772@gmail.com>
2023-09-04 12:25:20 +0500

    In generative AI prompt functinality added

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { CldImage } from "next-cloudinary"
 import { useState } from "react"
 
@@ -11,6 +13,9 @@ export default function EditPage({ searchParams: { publicId }, }:
     const [transformation, setTransformation] = useState<
         undefined | "generative-fill" | 'green' | 'blur' | 'grayscale' | 'pixelate' | 'border'
     >()
+
+    const [pendingPrompt, setPendingPrompt] = useState('')
+    const [prompt, setPrompt] = useState('')
 
     return (
         <section>
@@ -24,11 +29,14 @@ export default function EditPage({ searchParams: { publicId }, }:
                         className="rounded bg-white text-black hover:bg-white">
                         Clear All
                     </Button>
-
-                    <Button onClick={() => { setTransformation('generative-fill') }}
-                        className="rounded bg-white text-black hover:bg-white">
-                        Apply Generative Fill
-                    </Button>
+                    <div className="flex flex-col gap-4">
+                        <Button onClick={() => { setTransformation('generative-fill') }}
+                            className="rounded bg-white text-black hover:bg-white">
+                            Apply Generative Fill
+                        </Button>
+                        <Label>Prompt</Label>
+                        <Input value={pendingPrompt} onChange={(e) => setPendingPrompt(e.currentTarget.value)} />
+                    </div>
                     <Button onClick={() => { setTransformation('blur') }}
                         className="rounded bg-white text-black hover:bg-white">
                         Apply Blur
@@ -49,28 +57,25 @@ export default function EditPage({ searchParams: { publicId }, }:
                 </div>
 
                 <div className="grid grid-cols-2 gap-12">
-                    <CldImage
-                        src={publicId}
-                        width={300}
-                        height={200}
-                        alt="Some Image"
-                    />
+                    <CldImage src={publicId} width={300} height={200} alt="Some Image" />
 
                     {transformation === 'generative-fill' &&
                         <CldImage
                             src={publicId}
-                            width={300}
-                            height={200}
+                            width={1800}
+                            height={1400}
                             alt="Some Image"
                             crop="pad"
-                            fillBackground
+                            fillBackground={{
+                                    prompt,
+                                }}
                         />
                     }
                     {transformation === 'blur' &&
                         <CldImage
                             src={publicId}
-                            width={300}
-                            height={200}
+                            width={1200}
+                            height={1400}
                             blur={'1200'}
                             alt="Some Image"
                         />
@@ -78,8 +83,8 @@ export default function EditPage({ searchParams: { publicId }, }:
                     {transformation === 'grayscale' &&
                         <CldImage
                             src={publicId}
-                            width={300}
-                            height={200}
+                            width={1200}
+                            height={1400}
                             grayscale
                             alt="Some Image"
                         />
@@ -87,8 +92,8 @@ export default function EditPage({ searchParams: { publicId }, }:
                     {transformation === 'pixelate' &&
                         <CldImage
                             src={publicId}
-                            width={300}
-                            height={200}
+                            width={1200}
+                            height={1400}
                             pixelate
                             alt="Some Image"
                         />
@@ -96,8 +101,8 @@ export default function EditPage({ searchParams: { publicId }, }:
                     {transformation === 'border' &&
                         <CldImage
                             src={publicId}
-                            width={300}
-                            height={200}
+                            width={1200}
+                            height={1400}
                             border="40px_solid_purple"
                             alt="Some Image"
                         />
